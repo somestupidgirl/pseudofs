12e270bd59b8a4d08059212cc16bf8072ea70044
somestupidgirl <sunnevanattsol@gmail.com>
2022-01-12 17:08:27 +0000

    xnu_compat: define isdigit(d)

--- src/xnu_compat.h
+++ src/xnu_compat.h
@@ -25,6 +25,9 @@
 #define OFF_MAX                     LONG_MAX
 #define OFF_MIN                     LONG_MIN
 
+// From XNU osfmk/kern/printf.c, since we don't have FreeBSD include/ctype.h
+#define isdigit(d) ((d) >= '0' && (d) <= '9')
+
 /*
  * internal mount, proc and vnode structures
  *
